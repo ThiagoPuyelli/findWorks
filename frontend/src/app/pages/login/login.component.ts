b104fffaf1f83c8578ea723b99e2f1003e03ac22
ThiagoPuyelli <thiagopuyelli@gmail.com>
2021-02-20 04:01:58 -0300

    Login terminado

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { LoginService } from "../../services/login.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public dataForm: FormGroup
+
+  constructor(
+    private builder: FormBuilder,
+    private loginService: LoginService
+  ) {
+    this.dataForm = this.builder.group({
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(4)]]
+    })
+   }
 
   ngOnInit(): void {
+  }
+
+  sendLogin(){
+    event?.preventDefault();
+    const msgError: any = document.querySelector(".failed");
+
+    if(this.dataForm.status == "VALID"){
+      msgError.style.display = "none";
+      this.loginService.login(this.dataForm.value).subscribe(
+        (result: any) => {
+          if(!result.auth){
+            const msgNoAuth: any = document.querySelector(".noAuth");
+            msgNoAuth.style.display = "block";
+          } else {
+            console.log(result)
+          }
+        },
+        err => {
+          console.log(err);
+        }
+      )
+    } else {
+      msgError.style.display = "block";
+    }
   }
 
 }
